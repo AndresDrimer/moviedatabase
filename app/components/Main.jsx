93ba5d7ffc8765586c595c062d7d280ec295e36a
AndresDrimer <andresdrimer@hotmail.com>
2023-04-28 10:47:54 -0300

    quick fix selecting movies

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -154,7 +154,7 @@ function Main() {
         {/* contendor de las peliculas actuales */}
         <div className="w-full text-black grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5  2xl:max-w-[2200px] 2xl:mx-auto gap-8 px-8">
           {movies.map((it) => (
-            <div key={it.id} className="flex flex-col items-center cursor-pointer" onClick={()=>selectMovie(movie)}>
+            <div key={it.id} className="flex flex-col items-center cursor-pointer" onClick={()=>selectMovie(it)}>
               <Image
                 src={`${URL_IMAGE + it.poster_path}`}
                 alt="movie-poster"
